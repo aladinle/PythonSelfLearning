7c444af0715350bde7f6f4fd8e2de80b50149cc6
Trung Le <letrung87@gmail.com>
2025-10-14 09:10:05 -0700

    Trung: Clean up solutions

--- AI_Music_Finder_App/MusicFinder_backend/search/urls.py
+++ AI_Music_Finder_App/MusicFinder_backend/search/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import lyric_search
+from views import lyric_search # type: ignore
 
 urlpatterns = [
     path('search/', lyric_search, name='lyric_search'),
